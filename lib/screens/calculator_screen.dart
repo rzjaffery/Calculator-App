4b9ead921a53382f1398c08fd1b80b3c1fcb2ab7
Rayyan Zafar Jaffery <rzjaffery7@gmail.com>
2026-04-23 13:22:42 +0500

    Use CalculatorScreen as app entry point
    Add a new CalculatorScreen StatefulWidget (lib/screens/calculator_screen.dart) with basic input/result state and an empty Scaffold. Update lib/main.dart to import the new screen, rename MyApp to CalculatorApp, and set CalculatorScreen() as the MaterialApp home (also disable debug banner). Remove the generated counter/homepage demo code to prepare the project for the calculator UI.

---
+++ lib/screens/calculator_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class CalculatorScreen extends StatefulWidget {
+  const CalculatorScreen({super.key});
+
+  @override
+  State<CalculatorScreen> createState() => _CalculatorScreenState();
+}
+
+class _CalculatorScreenState extends State<CalculatorScreen> {
+
+  String input = "";
+  String result = "0";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
